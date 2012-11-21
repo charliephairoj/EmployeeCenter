5c8a3f5baf7a593774960dd139a014e49f036e07
Charlie Phairojmahakij <charliephairoj@gmail.com>
2012-11-21 09:28:07 +0700

    Add .gitignore

--- EmployeeCenter/wsgi.py
+++ EmployeeCenter/wsgi.py
@@ -14,14 +14,24 @@ framework.
 
 """
 import os
+import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EmployeeCenter.settings")
+path = '/home/ubuntu/employee_center'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EmployeeCenter.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
