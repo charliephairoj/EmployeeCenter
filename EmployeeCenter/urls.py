bcf9d5a10c23ecd8e4c3b64d8386ae1b4fe5f590
Charlie Phairojmahakij <charliephairoj@gmail.com>
2012-11-22 16:29:14 +0700

    Change url routing

--- EmployeeCenter/urls.py
+++ EmployeeCenter/urls.py
@@ -8,11 +8,9 @@ admin.autodiscover()
 
 
 #primary login and url routing
-urlpatterns = patterns('',
-    url(r'^$', 'login.views.appLogin'),
-    
-)
+
 urlpatterns = patterns('login.views',
+    url(r'^$', 'appLogin'),
     url(r'^login$', 'appLogin'),
     
 )
