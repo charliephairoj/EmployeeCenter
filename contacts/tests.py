bf689e5bf09ccceabd7d7df77dc5f571decb1075
Charlie Phairojmahakij <charliephairoj@gmail.com>
2013-09-27 10:03:59 +0700

    Added customer test

--- contacts/tests.py
+++ contacts/tests.py
@@ -39,14 +39,55 @@ class ContactResourceTest(ResourceTestCase):
         self.password = 'pass'
         self.user = User.objects.create_user(self.username, 'test@yahoo.com', self.password)
         
-        self.post_data = base_contact
+        self.customer = Customer(**base_contact)
+        self.customer.is_customer = True
+        self.customer.save()
         
+        self.address = Address(**base_address)
+        self.address.contact = self.customer
+        self.address.save()
+                
     def get_credentials(self):
         return self.create_basic(username=self.username, password=self.password)
     
-    def test_get(self):
-        print self.api_client.get('/api/v1/customer/', format='json')
+    def test_get_json_list(self):
+        """
+        Test GET of list 
+        """
+        #Retrieve and validate GET response
+        resp = self.api_client.get('/api/v1/customer/', format='json')
+        self.assertValidJSONResponse(resp)
+        
+        #test deserialized response
+        resp_obj = self.deserialize(resp)
+        self.assertEqual(len(resp_obj['objects']), 1)
+        customer = resp_obj['objects'][0]
+        self.assertEqual(customer["name"], 'Charlie Brown')
+        self.assertEqual(customer["first_name"], 'Charlie')
+        self.assertEqual(customer["last_name"], 'Brown')
+        self.assertEqual(customer["currency"], 'USD')
+        self.assertTrue(customer["is_customer"])
+        self.assertEqual(customer["email"], "charliep@dellarobbiathailand.com")
+        self.assertEqual(customer["telephone"], "08348229383")
+        self.assertEqual(customer["fax"], "0224223423")
+        
+    def test_post(self):
+        """
+        Test creating customer via post
+        """
+        #Validate resource creation
+        self.assertEqual(Customer.objects.count(), 1)
+        resp = self.api_client.post('/api/v1/customer/', 
+                                    format='json',
+                                    data=base_contact,
+                                    authentication=self.get_credentials())
+        self.assertEqual(Customer.objects.count(), 2)
+        
+        #Validated response to resource creation
+        
 
+        
+        
 
 
         
