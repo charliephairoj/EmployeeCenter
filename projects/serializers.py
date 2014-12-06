0c0d6bbd81f34d09d1a86172577865b557b5b56c
Charlie Phairojmahakij <charliephairoj@gmail.com>
2014-12-06 13:34:53 +0700

    Added serializer file for projects

---
+++ projects/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from projects.models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Project
+        field = ('id', 'codename')
