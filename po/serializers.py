7a796ea15189d6ee4b5763faa3ba1a677d243d8a
Charlie Phairojmahakij <charliephairoj@gmail.com>
2014-12-13 14:16:21 +0700

    Enable pdf creation for po

--- po/serializers.py
+++ po/serializers.py
@@ -149,7 +149,7 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
 
         instance.calculate_total()
         
-        #instance.create_and_upload_pdf()
+        instance.create_and_upload_pdf()
         
         instance.save()
         return instance
@@ -177,7 +177,7 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         
         instance.calculate_total()
         
-        #instance.create_and_upload_pdf()
+        instance.create_and_upload_pdf()
         
         instance.save()
         
