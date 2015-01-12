3be9e23cd39638260d0cf7f3efac3910ec64a9a3
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-01-12 14:01:57 +0700

    Fixed missing field names

--- hr/serializers.py
+++ hr/serializers.py
@@ -18,7 +18,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
                                                required=False, 
                                                allow_null=True)
     wage = serializers.DecimalField(required=False, decimal_places=2, max_digits=12, allow_null=True)
-    pay_period = serializers.CharField(required=False, allow_null=True)
+    pay_period = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     
     
     class Meta:
