121665f0918cee0e9f78b974e9d3a196861517f6
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-01-28 14:04:40 +0700

    add new equipment serializers file

---
+++ equipment/serializers.py
@@ -0,0 +1,67 @@
+import logging
+
+from rest_framework import serializers
+
+from equipment.models import Equipment
+from hr.models import Employee
+
+logger = logging.getLogger(__name__)
+
+
+class EquipmentListSerializer(serializers.ListSerializer):
+    def update(self, instance, validated_data):
+        """
+        Implmement multiple update for equipment
+        """
+        ret = []
+        #Update the quantity for each supply
+        for data in validated_data:
+            equipment = Equipment.objects.get(pk=data['id'])
+    
+            self.child.update(equipment, data)
+
+            ret.append(equipment)
+
+        return ret
+        
+        
+class EquipmentSerializer(serializers.ModelSerializer):
+    cost = serializers.DecimalField(max_digits=12, decimal_places=2, required=False)
+    employee = serializers.PrimaryKeyRelatedField(required=False,
+                                                  queryset=Employee.objects.all())
+    id = serializers.IntegerField(required=False)
+                                                  
+    class Meta:
+        model = Equipment
+        list_serializer_class = EquipmentListSerializer
+    
+    def update(self, instance, validated_data):
+        
+        instance = super(EquipmentSerializer, self).update(instance, validated_data)
+        
+        if instance.status.lower() == "checked in":
+            instance.employee = None
+        
+        instance.save()
+            
+        return instance
+        
+    def to_representation(self, instance):
+        """
+        Override 'to_represenation' method
+        """
+        ret = super(EquipmentSerializer, self).to_representation(instance)
+        
+        try:
+            ret['employee'] = {'id': instance.employee.id,
+                               'first_name': instance.employee.first_name,
+                               'last_name': instance.employee.last_name}
+        except AttributeError as e:
+            pass
+            
+        if ret['status'].lower() == 'checked in':
+            del ret['employee']
+                           
+        return ret
+        
+        
