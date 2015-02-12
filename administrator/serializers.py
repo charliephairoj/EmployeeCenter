7b47e47ed5f24d30c9490fa15d9f2ef0d8f6f3a7
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-02-11 23:04:43 -0800

    Begin migration of admin module to rest_framework

--- administrator/serializers.py
+++ administrator/serializers.py
@@ -13,23 +13,35 @@ class PermissionSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Permission
+    
         
-
 class GroupSerializer(serializers.ModelSerializer):
-    #permissions = PermissionSerializer(many=True)
+    permissions = PermissionSerializer(many=True, required=False)
+    name = serializers.CharField(required=False)
     
     class Meta:
         model = Group
         
              
 class UserSerializer(serializers.ModelSerializer):
-    #groups = GroupSerializer(many=True)
+    groups = GroupSerializer(many=True, required=False)
+    password = serializers.CharField(write_only=True, required=False)
     
     class Meta:
         model = User
-        #depth = 1
-        field = ['email']
-
+        read_only_fields = ['user_permissions']
+    
+    def update(self, instance, validated_data):
+        
+        client_groups = validated_data.pop('groups', [])
+        server_groups = instance.groups.all()
+        
+        logger.debug(client_groups)
+        logger.debug(server_groups)
+        
+        return instance
+        
+        
 
 
         
