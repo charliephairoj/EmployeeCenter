ba358cca642d09e0e3fb4c20befa2be7bbcedc8b
Charlie Phairojmahakij <charliephairoj@gmail.com>
2015-03-27 15:52:08 +0700

    Remove left over git marking

--- po/serializers.py
+++ po/serializers.py
@@ -1,4 +1,6 @@
+
 import logging
+import decimal
 from decimal import Decimal
 from datetime import datetime
 from pytz import timezone
@@ -19,7 +21,6 @@ class ItemSerializer(serializers.ModelSerializer):
     supply = serializers.PrimaryKeyRelatedField(queryset=Supply.objects.all())
     comments = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     description = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    
     class Meta:
         model = Item
         read_only_fields = ('total', 'sticker')
@@ -32,14 +33,12 @@ class ItemSerializer(serializers.ModelSerializer):
         supply = validated_data['supply']
         supply.supplier = self.context['supplier']
         purchase_order = self.context['po']
-        description = validated_data.pop('description', supply.description)
-        unit_cost = validated_data.pop('unit_cost', supply.cost)
-        discount = validated_data.pop('discount', supply.discount)
+        description = validated_data.pop('description', None) or supply.description
+        unit_cost = validated_data.pop('unit_cost', None) or supply.cost
+        discount = validated_data.pop('discount', None) or supply.discount
         
-        instance = self.Meta.model.objects.create(description=description,
-                                                  purchase_order=purchase_order,
-                                                  unit_cost=unit_cost,
-                                                  discount=discount, **validated_data)
+        instance = self.Meta.model.objects.create(description=description, purchase_order=purchase_order,
+                                                  unit_cost=unit_cost, **validated_data)
         instance.calculate_total()
         
         instance.save()
@@ -54,21 +53,21 @@ class ItemSerializer(serializers.ModelSerializer):
         Override the 'update' method
         """
         instance.supply.supplier = self.context['supplier']
-        instance.description = validated_data.pop('description', instance.description)
-        instance.unit_cost = validated_data.pop('unit_cost', instance.supply.cost)
+        instance.description = validated_data.pop('description', None) or instance.description
+        instance.unit_cost = validated_data.pop('unit_cost', None) or instance.supply.cost
         instance.quantity = Decimal(validated_data.get('quantity'))
-        instance.discount = validated_data.get('discount', instance.discount)
-        instance.comments = validated_data.get('comments', instance.comments)
+        instance.discount = validated_data.get('discount', None) or instance.discount
+        instance.comments = validated_data.get('comments', None) or instance.comments
         instance.calculate_total()
         instance.save()
         
-        # Check status change
+        #Check status change
         new_status = validated_data.get('status', instance.status)
         if new_status != instance.status and instance.status.lower() == "ordered":
             instance.status = new_status
             old_quantity = instance.supply.quantity
             
-            # Fix for if adding decimal and supply together
+            #Fix for if adding decimal and supply together
             try:
                 instance.supply.quantity += float(str(instance.quantity))
             except TypeError:
@@ -87,8 +86,8 @@ class ItemSerializer(serializers.ModelSerializer):
         """
         Method to change the cost of a supply
         
-        This will change the supply's product cost, respective of supplier,
-        in the database and will log the event as 'PRICE CHANGE'
+        This will change the supply's product cost, respective of supplier, in the database
+        and will log the event as 'PRICE CHANGE'
         """
         try:
             product = Product.objects.get(supply=supply, supplier=supply.supplier)
@@ -101,17 +100,16 @@ class ItemSerializer(serializers.ModelSerializer):
         product.cost = cost
         product.save()
         
-        message = u"Price change from {0}{2} to {1}{2} for {3} [Supplier: {4}]"
         log = Log(supply=supply,
                   supplier=supply.supplier,
                   action="PRICE CHANGE",
                   quantity=None,
                   cost=product.cost,
-                  message=message.format(old_price,
-                                         product.cost,
-                                         supply.supplier.currency,
-                                         supply.description,
-                                         supply.supplier.name))
+                  message=u"Price change from {0}{2} to {1}{2} for {3} [Supplier: {4}]".format(old_price,
+                                                                                              product.cost,
+                                                                                              supply.supplier.currency,
+                                                                                              supply.description,
+                                                                                              supply.supplier.name))
         log.save()
         
     def _log_quantity_change(self, obj, old_quantity, new_quantity, employee=None):
@@ -121,7 +119,7 @@ class ItemSerializer(serializers.ModelSerializer):
         """
         new_quantity = Decimal(str(new_quantity))
         
-        # Type change to ensure that calculations are only between Decimals
+        #Type change to ensure that calculations are only between Decimals
         old_quantity = Decimal(str(old_quantity))
         
         if new_quantity < 0:
@@ -135,34 +133,31 @@ class ItemSerializer(serializers.ModelSerializer):
                 action = 'SUBTRACT'
                 diff = old_quantity - new_quantity
 
-            # Create log to track quantity changes
-            message = u"{0}ed {1}{2} {3} {4}"
-            log = Log(supply=obj,
+            #Create log to track quantity changes
+            log = Log(supply=obj, 
                       action=action,
                       quantity=diff,
                       employee=employee,
-                      message=message.format(action.capitalize(),
-                                             diff,
-                                             obj.units,
-                                             "to" if action == "ADD" else "from",
-                                             obj.description))
+                      message=u"{0}ed {1}{2} {3} {4}".format(action.capitalize(),
+                                                             diff,
+                                                             obj.units,
+                                                             "to" if action == "ADD" else "from",
+                                                             obj.description))
             
-            # Save log
+            #Save log                                               
             log.save()
         
         
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     supplier = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all())
-    project = serializers.PrimaryKeyRelatedField(required=False, allow_null=True,
-                                                 queryset=Project.objects.all())
+    project = serializers.PrimaryKeyRelatedField(required=False, allow_null=True, queryset=Project.objects.all())
     items = ItemSerializer(many=True)
     order_date = serializers.DateTimeField(read_only=True)
     
     class Meta:
         model = PurchaseOrder
-        fields = ('vat', 'supplier', 'id', 'items', 'project', 'grand_total',
-                  'subtotal', 'total', 'revision', 'pdf', 'discount', 'status',
-                  'terms', 'order_date', 'currency', 'receive_date', 'paid_date')
+        fields = ('vat', 'supplier', 'id', 'items', 'project', 'grand_total', 'subtotal', 'total', 'revision', 'pdf', 
+                  'discount', 'status', 'terms', 'order_date', 'currency')
                  
         read_only_fields = ('pdf', 'revision')
         
@@ -172,7 +167,7 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         """
         ret = super(PurchaseOrderSerializer, self).to_representation(instance)
 
-        ret['supplier'] = {'id': instance.supplier.id,
+        ret['supplier'] = {'id': instance.supplier.id, 
                            'name': instance.supplier.name}
         
         try:
@@ -195,8 +190,8 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         """
-        Override the 'create' method to customize how items are created
-        and pass the supplier instance to the item serializer via context
+        Override the 'create' method to customize how items are created and pass the supplier instance
+        to the item serializer via context
         """
         items_data = validated_data.pop('items')
         for item_data in items_data:
@@ -205,20 +200,15 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
             except AttributeError:
                 item_data['supply'] = item_data['supply']['id']
                 
-        # Extract discount and terms from supplier of the purchase order
-        discount = validated_data.pop('discount', validated_data['supplier'].discount)
+        discount = validated_data.pop('discount', None) or validated_data['supplier'].discount
         terms = validated_data.pop('terms', validated_data['supplier'].terms)
         
-        # instance creation and attribute setting
-        instance = self.Meta.model.objects.create(employee=self.context['request'].user,
-                                                  discount=discount,
+        instance = self.Meta.model.objects.create(employee=self.context['request'].user, discount=discount,
                                                   **validated_data)
         instance.currency = instance.supplier.currency
         instance.terms = terms
         
-        item_serializer_context = {'supplier': instance.supplier, 'po': instance}
-        item_serializer = ItemSerializer(data=items_data,
-                                         context=item_serializer_context,
+        item_serializer = ItemSerializer(data=items_data, context={'supplier': instance.supplier, 'po':instance}, 
                                          many=True)
         if item_serializer.is_valid(raise_exception=True):
             item_serializer.save()
@@ -232,8 +222,7 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         
     def update(self, instance, validated_data):
         """
-        Override the 'update' method in order to increase the revision number and
-        create a new version of the pdf
+        Override the 'update' method in order to increase the revision number and create a new version of the pdf
         """
         
         status = validated_data.pop('status', "")
@@ -250,7 +239,7 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
             
             instance.save()
              
-        else:
+        else:  
             items_data = validated_data.pop('items')
             items_data = self.context['request'].data['items']
             for item_data in items_data:
@@ -280,14 +269,12 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
     
     def receive_order(self, instance, validated_data):
         """
-        Will received the order and then process the items and
-        the corresponding supplies. The quantities for the supplies
-        will automatically increase based on the supplies received
+        Will received the order and then process the items and the corresponding supplies.
+        The quantities for the supplies will automatically increase based on the supplies received
         """
         for item in instance.items.all():
             item.status = "RECEIVED"
-            
-            item = self._apply_new_quantity(item, instance)
+            self._apply_new_quantity(item, instance)
                 
             item.supply.save()
             item.save()
@@ -301,16 +288,19 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         
         return instance
         
+
     def _apply_new_quantity(self, item, po):
-        
+        # Retrieve product responding to this item and supplier
         product = Product.objects.get(supply=item.supply, 
                                       supplier=po.supplier)
         
+        #Calculate the quantity to add to current supply qty
         try:
             qty_to_add = Decimal(str(item.quantity)) * product.quantity_per_purchasing_unit
         except TypeError:
             qty_to_add = float(str(item.quantity)) * product.quantity_per_purchasing_unit
-            
+         
+        #Change the supply's current quantity   
         try:
             item.supply.quantity += Decimal(str(qty_to_add))
         except TypeError:
@@ -322,20 +312,16 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         """
         Handles creation, update, and deletion of items
         """
-        # Maps of id
+        #Maps of id
         id_list = [item_data.get('id', None) for item_data in items_data]
 
-        # Update or Create Item
+        #Update or Create Item
         for item_data in items_data:
             try:
                 
                 item = Item.objects.get(pk=item_data['id'])
                 item_data['purchase_order'] = item.id
-                serializer = ItemSerializer(item,
-                                            context={'supplier': instance.supplier},
-                                            data=item_data)
-                
-                # Save the data to the server
+                serializer = ItemSerializer(item, context={'supplier': instance.supplier}, data=item_data)
                 if serializer.is_valid(raise_exception=True):
                     item = serializer.save()
                 
@@ -353,15 +339,12 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
                 item.save()
                 """
             except KeyError:
-                item_serializer_context = {'supplier': instance.supplier, 'po': instance}
-                serializer = ItemSerializer(data=item_data,
-                                            context=item_serializer_context)
-                                            
+                serializer = ItemSerializer(data=item_data, context={'supplier': instance.supplier, 'po': instance})
                 if serializer.is_valid(raise_exception=True):
                     item = serializer.save()
                     id_list.append(item.id)
 
-        # Delete Items
+        #Delete Items
         for item in instance.items.all():
             if item.id not in id_list:
                 item.delete()
@@ -370,8 +353,8 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         """
         Method to change the cost of a supply
         
-        This will change the supply's product cost, respective of supplier,
-        in the database and will log the event as 'PRICE CHANGE'
+        This will change the supply's product cost, respective of supplier, in the database
+        and will log the event as 'PRICE CHANGE'
         """
         try:
             product = Product.objects.get(supply=supply, supplier=supply.supplier)
@@ -384,32 +367,30 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
         product.cost = cost
         product.save()
         
-        message = u"Price change from {0}{2} to {1}{2} for {3} [Supplier: {4}]"
         log = Log(supply=supply,
                   supplier=supply.supplier,
                   action="PRICE CHANGE",
                   quantity=None,
                   cost=product.cost,
-                  message=message.format(old_price,
-                                         product.cost,
-                                         supply.supplier.currency,
-                                         supply.description,
-                                         supply.supplier.name))
+                  message=u"Price change from {0}{2} to {1}{2} for {3} [Supplier: {4}]".format(old_price,
+                                                                                              product.cost,
+                                                                                              supply.supplier.currency,
+                                                                                              supply.description,
+                                                                                              supply.supplier.name))
         log.save()
         
     def _log_receiving_item(self, item):
         supply = item.supply
         supply.supplier = item.purchase_order.supplier
         
-        message = u"Received {0}{1} of {2} from {3}"
         log = Log(supply=item.supply,
                   supplier=item.purchase_order.supplier,
                   action="ADD",
                   quantity=item.quantity,
-                  message=message.format(item.quantity,
-                                         supply.purchasing_units,
-                                         supply.description,
-                                         item.purchase_order.supplier.name))
+                  message=u"Received {0}{1} of {2} from {3}".format(item.quantity,
+                                                                   supply.purchasing_units,
+                                                                   supply.description,
+                                                                   item.purchase_order.supplier.name))
         log.save()
         
     def _email_purchaser(self, purchase_order):
@@ -418,15 +399,14 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
             conn = boto.ses.connect_to_region('us-east-1')
             recipients = [purchase_order.employee.email]
             
-            # Build the email body
+            #Build the email body
             body = u"""<table><tr><td><h1>Purchase Order Received</h1></td><td></td><td></td></tr>
                              <tr><td>Purchase Order #</td><td>{0}</td><td></td></tr>
                              <tr><td>Supplier</td><td>{1}</td><td></td></tr>
                              <tr><td><h3>Description</h3></td><td><h3>Quantity</h3></td><td><h3>Status</h3></td</tr>
                    """.format(purchase_order.id,
                               purchase_order.supplier.name)
-                              
-            # Loop through all items to add to the body
+            #Loop through all items to add to the body
             for item in purchase_order.items.all():
                 supply = item.supply
                 color = u"green" if item.status.lower() == "received" else "red"
@@ -437,15 +417,12 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
                                    supply.purchasing_units,
                                    color,
                                    item.status)
-            # Closing table tag
-            body += u"</table>"
+            #Closing table tag
+            body += u"</table>"                
                     
-            # Send email
-            supplier_name = purchase_order.supplier.name
-            subject = u'Purchase Order from {0} Received'.format(supplier_name)
+            #Send email    
             conn.send_email('inventory@dellarobbiathailand.com',
-                            subject,
+                            u'Purchase Order from {0} Received'.format(purchase_order.supplier.name),
                             body,
                             recipients,
                             format='html')
-    
