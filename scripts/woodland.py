2326c3a84514d97c429d8335f80b0014f7751b34
Charlie Phairojmahakij <charliephairoj@gmail.com>
2016-01-06 10:05:12 +0700

    Implemented initial version of adding a calendar event for account payable of purchase order

--- scripts/woodland.py
+++ scripts/woodland.py
@@ -13,29 +13,91 @@ sys.path.append('/home/django_worker/backend')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'EmployeeCenter.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+import logging
+import httplib2
 
 import boto
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from oauth2client import xsrfutil
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.django_orm import Storage
+from apiclient import discovery
 
 from administrator.models import AWSUser
+from administrator.models import CredentialsModel
 
 
 django.setup()            
             
+logger = logging.getLogger(__name__)
+
+
 if __name__ == "__main__":
     
-    users = User.objects.all()
-    iam = boto.connect_iam()
+    user = User.objects.get(email="charliep@dellarobbiathailand.com")
     
-    for user in users:
-
-       
+    storage = Storage(CredentialsModel, 'id', user, 'credential')
+    credentials = storage.get()
+    
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+    response = service.calendarList().list().execute()
+    
+    calendar_summaries = [cal['summary'].lower() for cal in response['items']]
+    
+    
+    if 'account payables' not in calendar_summaries:
+        calendar = {
+            'summary': 'Account Payables',
+            'timeZone': 'Asia/Bangkok'
+        }
+        ap_cal = service.calendars().insert(body=calendar).execute()
         
-        response = iam.add_user_to_group('S3-Users', user.email)
-
-
-
-
-
+        service.calendarList().insert(body={
+            'id': ap_cal['id']
+        }).execute()
+    else:
+        for cal in response['items']:
+            if cal['summary'].lower() == 'account payables':
+                ap_cal = cal
+        
+    
+    response = service.acl().list(calendarId=ap_cal['id']).execute()
+    acl_users = [acl['scope']['value'] for acl in response['items']]
+    
+    group = Group.objects.get(name="Accountant")
+    
+    for user in group.user_set.all():
+        if user.email not in acl_users:
+            acl = {
+                'scope': {
+                    'type': 'user',
+                    'value': user.email
+                },
+                'role': 'owner'
+            }
+            response = service.acl().insert(body=acl, calendarId=ap_cal['id']).execute()
+            
+    logger.debug(service.acl().list(calendarId=ap_cal['id']).execute())
+    
+    evt = {
+        'summary': 'tes23t',
+        'start': {
+            'date': '2016-1-6'
+        },
+        'end': {
+            'date': '2016-1-6'
+        },
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'email', 'minutes': 10},
+            ]
+        }
+    }
+    response = service.events().insert(calendarId=ap_cal['id'], body=evt).execute()
+    
+    
 
 
