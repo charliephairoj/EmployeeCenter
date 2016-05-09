836775dacc8a7f0c536bf09656225c19db10b259
Charlie Phairojmahakij <charliephairoj@gmail.com>
2016-05-09 14:39:09 +0700

    Order fabrics retrieval by status. Change validation for last_contacted for deal to be datetime

--- supplies/views.py
+++ supplies/views.py
@@ -217,7 +217,7 @@ class FabricList(FabricMixin, SupplyList):
         """
         Override 'get_queryset' method in order to customize filter
         """
-        queryset = self.queryset.all()
+        queryset = self.queryset.all().order_by('status')
         
         #Filter based on query
         query = self.request.query_params.get('q', None)
