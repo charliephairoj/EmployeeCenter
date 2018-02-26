58ed4bdb4cfcaf426ed6066c83e2460e45ccf5c3
Charlie Phairojmahakij <charliephairoj@gmail.com>
2018-02-26 15:17:03 +0700

    Fixed missing discount error when creating a new quotaiton

--- estimates/views.py
+++ estimates/views.py
@@ -105,7 +105,7 @@ class EstimateMixin(object):
         Format fields that are primary key related so that they may 
         work with DRF
         """
-        fields = ['employee', 'fabric', 'items', 'customer', 'project']
+        fields = ['employee', 'fabric', 'items']
         
         for field in fields:
             if field in request.data:
@@ -147,7 +147,7 @@ class EstimateMixin(object):
         Format fields that are primary key related so that they may 
         work with DRF
         """
-        fields = ['employee', 'fabric', 'items', 'customer', 'project']
+        fields = ['employee', 'fabric', 'items']
         
         for field in fields:
             if field in request.data:
